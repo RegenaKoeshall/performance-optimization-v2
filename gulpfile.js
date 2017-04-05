23f1d1ef343c081308c5e510533e2943e9b23605
Regena Koeshall <regena.koeshall@gmail.com>
2017-04-05 11:23:25 -0400

    add gulp-clean-css
    added gulp-clean-css and made change to css link in the html

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,19 @@ var gulp = require('gulp'),
   concat = require('gulp-concat'),
   uglify = require('gulp-uglify'),
   rename = require('gulp-rename'),
+  minifyCss = require('gulp-clean-css'),
     maps = require('gulp-sourcemaps'),
      del = require('del');
 
 
-gulp.task('concatCss', function() {
-	return gulp.src('css/foundation.css')
-	.pipe(gulp.dest('css'));
+gulp.task('minifyCss', function(){
+    return gulp.src( 'css/foundation.css')
+        .pipe(maps.init())
+        .pipe(concat('styles.css'))
+        .pipe(minifyCss())
+        .pipe(rename('styles.min.css'))
+        .pipe(maps.write('./'))
+        .pipe(gulp.dest('css'));
 });
 
 gulp.task("concatScripts", function() {
@@ -26,13 +32,7 @@ gulp.task("concatScripts", function() {
     .pipe(gulp.dest('js'));
 });
 
-gulp.task('minifyCss', ['concatCss'], function() {
-	return gulp.src('css/foundation.css')
-	.pipe(uglify())
-	.pipe(rename(style.min.css))
-	.pipe(gulp.dest('css'));
 
-});
 gulp.task("minifyScripts", ["concatScripts"], function() {
   return gulp.src("js/app.js")
     .pipe(uglify())
@@ -42,16 +42,16 @@ gulp.task("minifyScripts", ["concatScripts"], function() {
 
 
 gulp.task('watchFiles', function() {
-	gulp.watch('css/foundation.css', ['minifyCss']);
+	gulp.watch('css/*.css', ['minifyCss']);
 	gulp.watch('js/*.js', ['minifyScripts']);
 });
 
 gulp.task('clean', function() {
-  del(['dist', 'css/foundation.css', 'js/app*.js*']);
+  del(['dist', 'css/styles.min.css', 'css/styles.min.css.map', 'js/app*.js*']);
 });
 
-gulp.task("build", ['minifyScripts'], function() {
-  return gulp.src(["css/foundation.css", "js/app.min.js", 'index.html',
+gulp.task("build", ['clean', 'minifyScripts', 'minifyCss'], function() {
+  return gulp.src(["css/styles.min.css", "js/app.min.js", 'index.html',
                    "img/**"], { base: './'})
             .pipe(gulp.dest('dist'));
 });
